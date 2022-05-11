6b510d702f5135745ef2f1a90ea0c5c8729494ec
Kevin La <la.kevin151@gmail.com>
2022-05-11 00:16:10 -0700

    Initial App Upload

---
+++ test/Color.test.js
@@ -0,0 +1,74 @@
+const { assert } = require('chai');
+
+const Color = artifacts.require('./Color.sol');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Color', (accounts) => {
+    let contract;
+
+    before(async() =>{
+        contract = await Color.deployed();
+    })
+
+    describe('deployment', async() => {
+        it('deploys successfully', async() => {
+            const address = contract.address;
+            assert.notEqual(address, '');
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+        })
+
+        it('has a name', async() =>{
+            const name = await contract.name();
+            assert.equal(name, 'Color');
+        })
+
+        it('has a symbol', async() =>{
+            const symbol = await contract.symbol();
+            assert.equal(symbol, 'COLOR');
+        })
+    })
+
+    describe("Minting", async() =>{
+
+        it('creates a new token', async() =>{
+            const result = await contract.mint('#FFFFFF');
+            const totalSupply = await contract.totalSupply();
+            
+            // Success
+            assert.equal(totalSupply, 1);
+            const event = result.logs[0].args;
+            assert.equal(event.tokenId.toNumber(),1, 'id is correct');
+            assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct');
+            assert.equal(event.to, accounts[0], 'to is correct');
+
+            await contract.mint('#FFFFFF').should.be.rejected;
+        })
+    })
+
+    describe('indexing', async() =>{
+
+        it('lists colors', async() =>{
+            await contract.mint("#000000");
+            await contract.mint("#5F5F5F");
+            await contract.mint("#3E3E3E");
+
+            const totalSupply = await contract.totalSupply();
+
+            let color;
+            let result = [];
+    
+            for(var i = 1; i <= totalSupply; i++){
+                color = await contract.colors(i-1);
+                result.push(color)
+            }
+
+            let expected = ['#FFFFFF', '#000000', '#5F5F5F', '#3E3E3E'];
+            assert.equal(result.join(','),expected.join(','))
+        })
+    })
+})
